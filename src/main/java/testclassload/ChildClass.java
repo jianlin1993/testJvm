30a0af724adb75b723449d9aa5bf9c0c83a6a656
Rain <1015894275@qq.com>
2019-04-01 14:51:21 +0800

    jvm2冲突测试

--- src/main/java/testclassload/ChildClass.java
+++ src/main/java/testclassload/ChildClass.java
@@ -1,6 +1,7 @@
 package testclassload;
 
 public class ChildClass extends SuperClass{
+    //jvm2冲突文件
     static {
         System.out.println("子类初始化！");
     }
--- src/main/java/testclassload/SuperClass.java
+++ src/main/java/testclassload/SuperClass.java
@@ -5,6 +5,7 @@ package testclassload;
  * 通过子类引用父类的静态字段  不会导致子类的初始化
  */
 public class SuperClass {
+    //jvm2不冲突文件
     static{
         System.out.println("父类初始化！");
     }
