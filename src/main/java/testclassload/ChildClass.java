9709ff45a76bab21d344d1a7690188df374edd07
Rain <1015894275@qq.com>
2019-04-01 14:54:55 +0800

    jvm冲突测试

--- src/main/java/testclassload/ChildClass.java
+++ src/main/java/testclassload/ChildClass.java
@@ -1,6 +1,7 @@
 package testclassload;
 
 public class ChildClass extends SuperClass{
+    //手动造一个冲突
     static {
         System.out.println("子类初始化！");
     }
--- src/test/java/test/TestClass.java
+++ src/test/java/test/TestClass.java
@@ -1,9 +1,10 @@
 package test;
 
 public class TestClass {
-
+//jvm1不冲突文件
 private int m;
 public int inc(){
+
     return m+1;
 }
 }
